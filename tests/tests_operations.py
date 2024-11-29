0633b0feb1efe2fd29f2250244ef544409c360b4
aakash.anand <aakash.anand@strandls.com>
2024-11-29 12:32:56 +0530

    changes in import

--- tests/tests_operations.py
+++ tests/tests_operations.py
@@ -1,6 +1,17 @@
+import sys
+import os
+
+# Add the project root directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 from src.maths_operation import add
 
 
 def test():
     assert add(4, 9) == 13
     assert add(-1, 1) == 0
+    print("All tests passed!")
+
+
+if __name__ == "__main__":
+    test()
